d554f7049cbed1f63ad3863119df9b1f7eba81a7
ANA AMARANTE <anapma22@gmail.com>
2020-07-15 20:34:03 -0300

    calculo do N0 da entrega 01.1

--- Práticas - Parte 2/Prática_03/Entrega_01.m
+++ Práticas - Parte 2/Prática_03/Entrega_01.m
@@ -8,8 +8,9 @@ Ts = 2;                       % Tempo de símbolo em portadora única
 K = T/Ts;                     % Número de subportadoras independentes
 N = 2*K;                      % DFT de N pontos
 EbN0dB = 0:1:14;              % Vetor de EbN0  (energy per bit to noise power spectral density ratio) is a normalized signal-to-noise ratio (SNR) measure
-Eb = 0;
-N0 = Eb * 10.^(-EbN0dB/10);  % Densidade espectral do ruído
+Eb = 1;                       % Energia de bit para o BPSK
+N0 = Eb * 10.^(-EbN0dB/10);   % Potência de ruído (Proakis)
+variance = 0;
 
 % Gerar bits aleatórios para 16-QAM
 dataIn=rand(1,n_bits);   % Sequência de números entre 0 e 1 uniformemente distribuídos
@@ -17,11 +18,11 @@ dataIn=sign(dataIn-.5);  % Sequência de -1 e 1
 % Conversor serial paralelo
 dataInMatrix = reshape(dataIn,n_bits/4,4); % Conversão serial para paralelo
 
-% Gerar bits aleatórios para BPSK
-dataIn_bpsk=rand(1,n_bits);   % Sequência de números entre 0 e 1 uniformemente distribuídos
-dataIn_bpsk=sign(dataIn-.5);  % Sequência de -1 e 1
-% Conversor serial paralelo
-dataInMatrix_bpsk = reshape(dataIn,n_bits/4,4); % Conversão serial para paralelo
+% % Gerar bits aleatórios para BPSK
+% dataIn_bpsk=rand(1,n_bits);   % Sequência de números entre 0 e 1 uniformemente distribuídos
+% dataIn_bpsk=sign(dataIn-.5);  % Sequência de -1 e 1
+% % Conversor serial paralelo
+% dataInMatrix_bpsk = reshape(dataIn,n_bits/4,4); % Conversão serial para paralelo
 
 % Gerar constelaçao 16-QAM
 seq16qam = 2*dataInMatrix(:,1)+dataInMatrix(:,2)+1i*(2*dataInMatrix(:,3)+dataInMatrix(:,4)); 
@@ -41,8 +42,8 @@ end
 % Loop de variâncias
 for ik = 1:length(EbN0dB)
     % Adição de ruído
-    noise = sqrt(N0)*randn(1,N)+1i*sqrt(N0)*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal
-    noise_bpsk = sqrt(NO)*randn(1,N)+1i*sqrt(N0)*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal
+    noise = sqrt(N0(ik))*randn(1,N)+1i*sqrt(N0(ik))*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal
+%     noise_bpsk = sqrt(N0(ik))*randn(1,N)+1i*sqrt(N0)*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal
     %
     rn = xn + noise; % sinal recebido amostrado 
     % Fim da geracao do ruído AWGN complexo
@@ -60,7 +61,7 @@ for ik = 1:length(EbN0dB)
     hold on
     scatter(real(seq16),imag(seq16), 'r', '+') % Cruzes vermelhas, é a constelação 16-QAM 
     hold off
-    title(['Sinal com ruído de variância ', num2str(variance)]);
+    title(['Sinal com Eb/N0 de ', num2str(EbN0dB(ik)), 'dB']);
     
     % Demodulação, isolando a parte real e imaginário de Y e comparando com os valores das regiões da constelação do 16-QAM
     for k= 1:length(Y) % Para percorrer todo o vetor Yk 
@@ -102,5 +103,6 @@ for ik = 1:length(EbN0dB)
     % Contagem de erro
     % find(X) returns a vector containing the linear indices of each nonzero element in array X.
     error = length(find(Z(1,2:K)-X(1,2:K)));
-    disp(['Para variância de ', num2str(variance), ' houve ', num2str(error), ' símbolos errados.']);
+    disp(['Para EbN0 de ', num2str(EbN0dB(ik)),'dB, a variância é ', num2str(variance)]);
+    %disp(['Para Eb/no de 'num2str(EbN0dB), ' temos a variância de ', num2str(variance), ' houve ', num2str(error), ' símbolos errados.']);
 end
