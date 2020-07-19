433c1d592b50a8241b4310dba704e713cd6910ee
ANA AMARANTE <anapma22@gmail.com>
2020-07-19 02:37:22 -0300

    testes na 1 parte 2

--- Entregas_Parte_02_Ana_Amarante/Entrega01_3.m
+++ Entregas_Parte_02_Ana_Amarante/Entrega01_3.m
@@ -1,5 +1,6 @@
-% 1.1 Variar a Eb/No de 0 a 14 dB e calcular a variância do ruído, considerando modulação BPSK e 16-QAM;
-% 1.2 Usar as funções ifft e fft para multiplexar (Tx) e demultiplexar (Rx);
+%1.3 Fazer o gráfico da BER vs Eb/No para com OFDM e, no mesmo gráfico, o gráfico da 
+% Pe vs Eb/No (fórmula teórica)  da modulação BPSK e 16-QAM sem OFDM.
+
 clear all; clc; close all
 
 % Parâmetros com valores fixos
@@ -9,11 +10,31 @@ Ts = 2;                       % Tempo de símbolo em portadora única
 K = T/Ts;                     % Número de subportadoras independentes
 N = 2*K;                      % DFT de N pontos
 EbNodB = 0:1:14;              % Vetor de EbNo2  (energy per bit to noise power spectral density ratio) is a normalized signal-to-noise ratio (SNR) measure
+EbNo = 10.^(EbNodB/10);      % Eb/No em escala linear
 Eb_bpsk = 1;                  % Energia de bit para o BPSK
 No2_bpsk = Eb_bpsk *10.^(-EbNodB/10);   % Potência de ruído (Proakis)
+Nbps = [1 4];                 % Bits por segundo que varia para cada modulação
+M = 2.^Nbps;
+fs= 1/Ts;
 
-for ik = 1:length(EbNodB) 
-    %------------- 16-QAM --------------------------------
+for i=1:length(Nbps) 
+    if Nbps(i) == 1
+        Pe = qfunc(sqrt(2*EbNo)); %(https://www.mathworks.com/help/comm/ref/qfunc.html)
+        theoreticalBER = Pe;
+        figure (1)
+        plot(EbNodB,log10(theoreticalBER),'c*');
+        hold on
+    else
+        Pe = 2.*(1-(1/sqrt(M(i)))).*erfc(sqrt(3*EbNo/(2*(M(i)-1))));
+        theoreticalBER = 1/2.*Pe;
+        figure (1)
+        plot(EbNodB,log10(theoreticalBER),'m*');
+        hold on
+    end
+end
+
+%------------- 16-QAM --------------------------------
+ for ik = 1:length(EbNodB) 
     % Gerar bits aleatórios para 16-QAM
     dataIn=rand(1,n_bits);   % Sequência de números entre 0 e 1 uniformemente distribuídos
     dataIn=sign(dataIn-.5);  % Sequência de -1 e 1
@@ -39,7 +60,8 @@ for ik = 1:length(EbNodB)
     end
     Eb = (1/sqrt(10))*dataIn; % normalization of energy to 1 (https://www.mathworks.com/matlabcentral/fileexchange/19403-symbol-error-rate-for-16qam-in-awgn-channel)
     No2 = Eb *10.^(-EbNodB(ik)/10);   % Potência de ruído (Proakis)
-    noise = sqrt(No2(ik))*randn(1,N)+1i*sqrt(No2(ik))*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal
+    noise = sqrt((No2(ik))/2)*randn(1,N)+1i*sqrt((No2(ik))/2)*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal
+    %noise=1/sqrt(2)*(randn(1,length(ofdm_signal))+1i*randn(1,length(ofdm_signal)));
     % Sinal recebido amostrado para 16-QAM
     rn = xt + noise;   
     % Discrete Fourier Transform (DFT) de rn para 16-QAM - Demodulação
@@ -77,13 +99,27 @@ for ik = 1:length(EbNodB)
         end
     end
     % Cálculo de variância para 16-QAM
-    variance = sum(find(Z(1,2:K)-X(1,2:K)))/n_bits;
-    disp(['Modulação 16-QAM: Para Eb/No de ', num2str(EbNodB(ik)),'dB, a variância é ', num2str(variance)]);
+    variance = var(noise(:));%sum(find(Z(1,2:K)-X(1,2:K)))/n_bits;
+
+    disp(['16-QAM: Para Eb/No de ', num2str(EbNodB(ik)),'dB, a variância é ', num2str(variance)]);
     % Contagem de erro para 16-QAM
     error = length(find(Z(1,2:K)-X(1,2:K))); % find(X) returns a vector containing the linear indices of each nonzero element in array X.
-    %---------------------------------------------------
-    
-    %------------------- BPSK --------------------------
+    BER(ik)=error/n_bits; % Calculo da BER 
+    %     Plots para 16-QAM
+%     scatterplot(Y) % Círculos azuis, é a constelação de Yk
+%     hold on
+%     scatter(real(seq16),imag(seq16), 'r', '+') % Cruzes vermelhas, é a constelação 16-QAM 
+%     hold off
+%     title(['Modulação 16-QAM: Sinal com Eb/No de ', num2str(EbNodB(ik)), 'dB']);
+ end
+ 
+figure(1)
+plot(EbNodB,log10(BER),'b--o');
+hold on
+%---------------------------------------------------
+ 
+%------------------- BPSK --------------------------
+for ik = 1:length(EbNodB) 
     % Gerar bits aleatórios para BPSK
     dataIn_bpsk=rand(1,n_bits);   % Sequência de números entre 0 e 1 uniformemente distribuídos
     dataIn_bpsk=sign(dataIn_bpsk-.5);  % Sequência de -1 e 1
@@ -104,10 +140,10 @@ for ik = 1:length(EbNodB)
     % Construindo xt para 16-QAM, para o TX
     xt_bpsk=zeros(1, T+1);
     for t=0:T
-        xt_bpsk=ifft(xn_bpsk,[],6); %(https://www.mathworks.com/help/matlab/ref/ifft.html)
+        xt_bpsk=ifft(xn_bpsk,[],N); %(https://www.mathworks.com/help/matlab/ref/ifft.html)
         % returns the inverse Fourier transform along the dimension dim. For example, if Y is a matrix, then ifft(Y,n,2) returns the n-point inverse transform of each row.
     end
-    noise_bpsk = sqrt(No2(ik))*randn(1,N)+1i*sqrt(No2(ik))*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal   
+    noise_bpsk = sqrt((No2(ik))/2)*randn(1,N)+1i*sqrt((No2(ik))/2)*randn(1,N); % Influência no eixo imaginário e real, isto é, influencia na amplitude e na fase do sinal   
     % Sinal recebido amostrado para BPSK
     rn_bpsk = xt_bpsk + noise_bpsk; % sinal recebido amostrado  para bpsk
     % Discrete Fourier Transform (DFT) de rn para BKPS - Demodulação
@@ -124,18 +160,12 @@ for ik = 1:length(EbNodB)
         end
     end
     % Cálculo de variância para BPSK
-    variance_bpsk = sum(find(Z_bpsk(1,2:K)-X_bpsk(1,2:K)))/n_bits;    
-    disp(['Modulação BPSK:   Para Eb/No de ', num2str(EbNodB(ik)),'dB, a variância é ', num2str(variance_bpsk)]);   
+    variance_bpsk = var(noise_bpsk(:)); %sum(find(Z_bpsk(1,2:K)-X_bpsk(1,2:K)))/n_bits;    
+    disp(['BPSK:   Para Eb/No de ', num2str(EbNodB(ik)),'dB, a variância é ', num2str(variance_bpsk)]);   
     % Contagem de erro para BPSK
-    error = length(find(Z(1,2:K)-X(1,2:K))); % find(X) returns a vector containing the linear indices of each nonzero element in array X.
-    
-%     Plots para 16-QAM
-%     scatterplot(Y) % Círculos azuis, é a constelação de Yk
-%     hold on
-%     scatter(real(seq16),imag(seq16), 'r', '+') % Cruzes vermelhas, é a constelação 16-QAM 
-%     hold off
-%     title(['Modulação 16-QAM: Sinal com Eb/No de ', num2str(EbNodB(ik)), 'dB']);
-    
+    error_bpsk = length(find(Z(1,2:K)-X(1,2:K)));
+    BER_bpsk(ik)=error_bpsk/n_bits; % Calculo da BER
+        
 %     Plots BPSK
 %     scatterplot(Y_bpsk)
 %     hold on
@@ -144,3 +174,13 @@ for ik = 1:length(EbNodB)
 %     hold off
 %     title(['Modulação BPSK: Sinal com Eb/No de ', num2str(EbNodB(ik)), 'dB']);
 end
+
+figure(1)
+plot(EbNodB,log10(BER_bpsk),'r--o');
+hold on
+grid on, hold on;
+xlabel('Eb/No (dB)')
+ylabel('BER')
+legend({'BER teórica - BPSK sem OFDM','BER teórica - 16-QAM sem OFDM','BER simulada - 16-QAM com OFDM','BER simulada - BPSK com OFDM'},'Location','southwest');
+%---------------------------------------------------
+    
